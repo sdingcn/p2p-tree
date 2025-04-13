98cf4699c870cfc417aa819ed6717f5d74602462
Shuo Ding <sdingcn@outlook.com>
2025-04-13 20:03:00 +0800

    added more tests and refined the test script

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ connections with its invited members and the member that invited it.
 The first member of a group has no inviter.
 A group's topology is a tree and messages are relayed by nodes on the tree.
 
-## Usage
+## usage
 
 There are two modes of usage: GUI and CLI.
 GUI supports one-line messages (including empty lines)
@@ -16,16 +16,20 @@ while CLI only supports non-empty one-line messages
 because empty lines are used to signal termination.
 Run `python3 node.py` to see the detailed arguments to launch a node.
 
-## Dependency
+## tests
+
+`python3 test.py`
+
+## dependency
 
 + Python >= 3.10
 + IPv4
 
-## Packets
+## packets
 
 Packets (over TCP) are of fixed-length (128 bytes).
 
-## Stability
+## stability
 
 The disconnection of one node may split the group (tree)
 into multiple smaller groups.
--- test.py
+++ test.py
@@ -27,7 +27,16 @@ def verify_err_line(p: subprocess.Popen, s: str) -> None:
 def nap() -> None:
     time.sleep(1)
 
-def p2p_tree_test_one_node() -> bool:
+def finalize(*ps: subprocess.Popen) -> None:
+    sys.stderr.write(f'finalizing {len(ps)} processes...\n')
+    for p in ps:
+        if p.poll() is None:
+            write_line(p, '')  # first ask the process to terminate
+            nap()
+            if p.poll() is None:  # if the process still doesn't terminate then kill it
+                p.kill()
+
+def p2p_tree_test_1() -> bool:
     try:
         a = launch(['A', '127.0.0.1', '10001'])
         nap()
@@ -40,9 +49,10 @@ def p2p_tree_test_one_node() -> bool:
             raise P2PTreeTestError()
         return True
     except P2PTreeTestError:
+        finalize(a)
         return False
 
-def p2p_tree_test_two_nodes() -> bool:
+def p2p_tree_test_2() -> bool:
     try:
         a = launch(['A', '127.0.0.1', '10001'])
         nap()
@@ -69,12 +79,60 @@ def p2p_tree_test_two_nodes() -> bool:
             raise P2PTreeTestError()
         return True
     except P2PTreeTestError:
+        finalize(a, b)
+        return False
+
+def p2p_tree_test_3() -> bool:
+    try:
+        a = launch(['A', '127.0.0.1', '10001'])
+        nap()
+        b = launch(['B', '127.0.0.1', '10002', '127.0.0.1', '10001'])
+        nap()
+        verify_err_line(a, 'accepted')
+        verify_err_line(b, 'connected')
+        c = launch(['C', '127.0.0.1', '10003', '127.0.0.1', '10002'])
+        nap()
+        verify_err_line(b, 'accepted')
+        verify_err_line(c, 'connected')
+        write_line(a, 'This is A')
+        verify_out_line(a, 'This is A')
+        verify_out_line(b, 'This is A')
+        verify_out_line(c, 'This is A')
+        write_line(b, 'This is B')
+        verify_out_line(a, 'This is B')
+        verify_out_line(b, 'This is B')
+        verify_out_line(c, 'This is B')
+        write_line(c, 'This is C')
+        verify_out_line(a, 'This is C')
+        verify_out_line(b, 'This is C')
+        verify_out_line(c, 'This is C')
+        write_line(b, '')
+        verify_err_line(a, 'detected')
+        verify_err_line(c, 'detected')
+        nap()
+        if b.poll() is None:
+            b.kill()
+            raise P2PTreeTestError()
+        write_line(a, '')
+        nap()
+        if a.poll() is None:
+            a.kill()
+            raise P2PTreeTestError()
+        write_line(c, '')
+        nap()
+        if c.poll() is None:
+            c.kill()
+            raise P2PTreeTestError()
+        return True
+    except P2PTreeTestError:
+        finalize(a, b, c)
         return False
 
 if __name__ == '__main__':
     global_dict = globals().copy()
     for k, v in global_dict.items():
         if k.startswith('p2p_tree_test'):
+            print(f'running test {k}...')
             if v():
                 print(f'test {k} passed')
             else:
