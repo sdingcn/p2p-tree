5a6dce259a53b4620ff76fb994f790064d86c8ca
Shuo Ding <sdingcn@outlook.com>
2025-04-18 23:02:30 +0800

    better code and more tests

--- node.py
+++ node.py
@@ -17,18 +17,14 @@ HALTED = False
 
 def make_header(name: typing.Union[None, str] = None) -> str:
     '''
-    Every message printed to the console has a header
-    containing the message sender and the sending time.
+    sender and time
     '''
-    if name is None:
-        name = 'system'
-    else:
-        name = f'[{name}]'
-    time_str = datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d %H:%M:%S')
-    return f'({name} {time_str}) '
+    name = 'system' if name is None else f'[{name}]'
+    time_string = datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d %H:%M:%S')
+    return f'({name} {time_string}) '
 
-def dump_to_stderr(s: str) -> None:
-    sys.stderr.write(make_header() + s)
+def dump_to_stderr(message: str) -> None:
+    sys.stderr.write(message)
     sys.stderr.flush()
 
 def make_packet(data: bytes) -> bytes:
@@ -46,126 +42,133 @@ class Neighbor:
     Each neighbor object contains:
     (1) a read buffer containing data received from the corresponding neighbor node,
     (2) a write buffer containing data to be sent to the corresponding neighbor node.
+    All buffers append new data to the right end.
     '''
-    def __init__(self, so: socket.socket, remote_addr: tuple[str, int],
+    def __init__(self, the_socket: socket.socket, remote_address: tuple[str, int],
                  read_buffer: bytes, write_buffer: bytes):
-        self.so = so
-        self.remote_addr = remote_addr
+        self.the_socket = the_socket
+        self.remote_address = remote_address
         self.read_buffer = read_buffer
         self.write_buffer = write_buffer
 
     def __str__(self):
-        return (f'Neighbor {id(self.so)} {self.so.getsockname()} {self.remote_addr} ' +
-                f'{self.read_buffer} {self.write_buffer}')
+        return (f'Neighbor {id(self.the_socket)} {self.the_socket.getsockname()} '
+                f'{self.remote_address} {self.read_buffer} {self.write_buffer}')
 
 def listener(lock: threading.Lock, server_socket: socket.socket,
              neighbors: list[Neighbor]) -> None:
     global HALTED
-    round_ctr = 0
-    accept_ctr = 0
+    round_counter = 0
+    accept_counter = 0
     while True:
-        round_ctr += 1
         # check the halting condition
         with lock:
             if HALTED:
-                dump_to_stderr(f' listener rounds = {round_ctr}'
-                               f' listener accepts = {accept_ctr}\n')
+                dump_to_stderr(make_header() + f' listener rounds = {round_counter}' +
+                               f' listener accepts = {accept_counter}\n')
                 return
         # try to accept new connections
         try:
-            client_socket, client_addr = server_socket.accept()
+            client_socket, client_address = server_socket.accept()
             client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             client_socket.settimeout(SOCKET_OPERATION_TIMEOUT_SECONDS)
             with lock:
-                neighbors.append(Neighbor(client_socket, client_addr, b'', b''))
-                dump_to_stderr(f'accepted a new connection from {client_addr}\n')
-            accept_ctr += 1
+                neighbors.append(Neighbor(client_socket, client_address, b'', b''))
+                dump_to_stderr(make_header() +
+                               f'accepted a new connection from {client_address}\n')
+            accept_counter += 1
         except TimeoutError:
             pass
+        # round counter update
+        round_counter += 1
 
 def relayer(lock: threading.Lock, neighbors: list[Neighbor],
             output: typing.Callable[[str], None]) -> None:
     global PACKET_LENGTH_BYTES, HALTED
-    round_ctr = 0
-    read_ctr = 0
-    write_ctr = 0
+    round_counter = 0
+    read_counter = 0
+    write_counter = 0
     while True:
-        round_ctr += 1
         # avoid busy waiting
         time.sleep(SOCKET_OPERATION_TIMEOUT_SECONDS)
         # check the halting condition
         with lock:
             if HALTED:
-                dump_to_stderr(f' relayer rounds = {round_ctr}'
-                               f' relayer read bytes = {read_ctr}'
-                               f' relayer write bytes = {write_ctr}\n')
+                dump_to_stderr(make_header() + f' relayer rounds = {round_counter}' +
+                               f' relayer read bytes = {read_counter}' +
+                               f' relayer write bytes = {write_counter}\n')
                 return
-        # obtain a list of current sockets
+        # freeze and obtain a list of current sockets
         # note: the listener may add new neighbor objects to "neighbors"
-        # during this round of relay
+        # during this round of relayer
         with lock:
-            so_list = [neighbor.so for neighbor in neighbors]
-        if so_list:
-            readable_so_list, writable_so_list, exceptional_so_list = select.select(
-                so_list, so_list, so_list, SELECT_TIMEOUT_SECONDS)
+            socket_list = [neighbor.the_socket for neighbor in neighbors]
+        if socket_list:
+            readable_socket_list, writable_socket_list, exceptional_socket_list = select.select(
+                socket_list, socket_list, socket_list, SELECT_TIMEOUT_SECONDS)
         else:
             continue
-        # read and detect dead sockets
-        dead_so_list = []
+        # prepare variables
         reads = {}
-        for so in readable_so_list:
-            data = so.recv(PACKET_LENGTH_BYTES)
+        writes = {}
+        writable_socket_id_set = set([id(the_socket) for the_socket in writable_socket_list])
+        # read data and detect dead sockets
+        dead_socket_list = []
+        for the_socket in readable_socket_list:
+            data = the_socket.recv(PACKET_LENGTH_BYTES)
             if data == b'':
-                dead_so_list.append(so)
+                dead_socket_list.append(the_socket)
             else:
-                reads[id(so)] = data
-                read_ctr += len(data)
-        # save reads to buffers, print whole packets, move printed packets to relay buffers
+                reads[id(the_socket)] = data
+                read_counter += len(data)
+        # save reads to buffers, print whole packets, relay printed packets, collect writes
         with lock:
             for neighbor in neighbors:
-                sid = id(neighbor.so)
-                if sid in reads:
-                    neighbor.read_buffer += reads[sid]
+                socket_id = id(neighbor.the_socket)
+                if socket_id in reads:
+                    neighbor.read_buffer += reads[socket_id]
                 if len(neighbor.read_buffer) >= PACKET_LENGTH_BYTES:
                     packet = neighbor.read_buffer[:PACKET_LENGTH_BYTES]
                     neighbor.read_buffer = neighbor.read_buffer[PACKET_LENGTH_BYTES:]
-                    output(packet.decode().strip() + '\n')
+                    message = packet.decode().strip() + '\n'
+                    output(message)
                     for other in neighbors:
-                        if id(other.so) != sid:
+                        if id(other.the_socket) != socket_id:  # to be sent to "other" neighbors
                             other.write_buffer += packet
-        # get writable data
-        writes = {}
-        with lock:
             for neighbor in neighbors:
-                sid = id(neighbor.so)
-                if neighbor.write_buffer:
-                    writes[sid] = neighbor.write_buffer
+                socket_id = id(neighbor.the_socket)
+                if (socket_id in writable_socket_id_set) and neighbor.write_buffer:
+                    writes[socket_id] = neighbor.write_buffer
                     neighbor.write_buffer = b''
-        # try to write everything
-        for so in writable_so_list:
-            sid = id(so)
-            if sid in writes:
-                sent = so.send(writes[sid])
-                writes[sid] = writes[sid][sent:]
-                write_ctr += sent
+        # write data
+        for the_socket in writable_socket_list:
+            socket_id = id(the_socket)
+            if socket_id in writes:
+                sent = the_socket.send(writes[socket_id])
+                writes[socket_id] = writes[socket_id][sent:]
+                write_counter += sent
         # store back not-written parts
         with lock:
             for neighbor in neighbors:
-                sid = id(neighbor.so)
-                if sid in writes:
-                    neighbor.write_buffer = writes[sid] + neighbor.write_buffer
+                socket_id = id(neighbor.the_socket)
+                if socket_id in writes:
+                    # prepend the old data to the beginning
+                    neighbor.write_buffer = writes[socket_id] + neighbor.write_buffer
         # detect disconnections and update the neighbor list
-        dead_sids = set([id(so) for so in dead_so_list] +
-                        [id(so) for so in exceptional_so_list])
+        dead_socket_id_set = set([id(the_socket) for the_socket in dead_socket_list] +
+                                 [id(the_socket) for the_socket in exceptional_socket_list])
         with lock:
             remaining_neighbors = []
             for neighbor in neighbors:
-                if id(neighbor.so) in dead_sids:
-                    dump_to_stderr(f'detected the disconnection of {neighbor.remote_addr}\n')
+                if id(neighbor.the_socket) in dead_socket_id_set:
+                    dump_to_stderr(make_header() +
+                                   f'detected the disconnection of {neighbor.remote_address}\n')
                 else:
                     remaining_neighbors.append(neighbor)
             neighbors.clear()
-            neighbors.extend(remaining_neighbors)
+            neighbors.extend(remaining_neighbors)  # cannot use '=' because that's re-binding
+        # round counter update
+        round_counter += 1
 
 def stop_and_wait(lock: threading.Lock,
                  listener_thread: typing.Union[None, threading.Thread],
@@ -183,45 +186,43 @@ def gui_loop(name: str, neighbors: list[Neighbor], lock: threading.Lock,
     root = tkinter.Tk()
     root.title('p2p-tree')
     root.resizable(False, False)
-    # root.attributes('-alpha', 0.8)
     root.rowconfigure(0, weight = 1)
     root.columnconfigure(0, weight = 1)
-    frm = tkinter.ttk.Frame(root, padding = 5, borderwidth = 5, relief = 'ridge')
-    frm.grid(row = 0, column = 0, padx = 5, pady = 5)
-    frm.rowconfigure(0, weight = 1)
-    frm.rowconfigure(1, weight = 1)
-    frm.columnconfigure(0, weight = 1)
-    display_area = tkinter.scrolledtext.ScrolledText(frm)
+    frame = tkinter.ttk.Frame(root, padding = 5, borderwidth = 5, relief = 'ridge')
+    frame.grid(row = 0, column = 0, padx = 5, pady = 5)
+    frame.rowconfigure(0, weight = 1)
+    frame.rowconfigure(1, weight = 1)
+    frame.columnconfigure(0, weight = 1)
+    display_area = tkinter.scrolledtext.ScrolledText(frame)
     display_area.bind('<Key>', lambda e: 'break')
     display_area.grid(row = 0, column = 0, sticky = 'NSEW')
     entry_variable = tkinter.StringVar()
-    entry_area = tkinter.Entry(frm, textvariable = entry_variable, borderwidth = 3,
+    entry_area = tkinter.Entry(frame, textvariable = entry_variable, borderwidth = 3,
                                relief = 'ridge')
     entry_area.grid(row = 1, column = 0, sticky = 'NSEW')
 
+    def gui_output(message: str) -> None:
+        nonlocal display_area
+        display_area.insert(tkinter.INSERT, message)
+
     def handle_input(event: tkinter.Event) -> None:
-        nonlocal display_area, entry_variable, name, neighbors, lock
+        nonlocal name, neighbors, lock, display_area, entry_variable
         entry_text = entry_variable.get()
         entry_variable.set('')
-        message = make_header(name) + entry_text
+        message = make_header(name) + entry_text + '\n'
         data = make_packet(message.encode())
         with lock:
-            display_area.insert(tkinter.INSERT, message + '\n')
+            gui_output(message)
             for neighbor in neighbors:
                 neighbor.write_buffer += data  # append data to all neighbor nodes' buffers
 
     entry_area.bind('<Key-Return>', handle_input)
-
-    def gui_output(s: str) -> None:
-        nonlocal display_area
-        display_area.insert(tkinter.INSERT, s)
-
-    # start the current node's relayer part
+    # start the relayer
     relayer_thread = threading.Thread(target = relayer, args = (lock, neighbors, gui_output))
     relayer_thread.start()
 
     def exit_loop() -> None:
-        nonlocal lock, listener_thread, relayer_thread, root
+        nonlocal lock, listener_thread, root, relayer_thread
         stop_and_wait(lock, listener_thread, relayer_thread)
         root.destroy()
 
@@ -232,48 +233,47 @@ def gui_loop(name: str, neighbors: list[Neighbor], lock: threading.Lock,
 def cli_loop(name: str, neighbors: list[Neighbor], lock: threading.Lock,
              listener_thread: typing.Union[None, threading.Thread]) -> None:
 
-    def cli_output(s: str) -> None:
-        sys.stdout.write(s)
+    def cli_output(message: str) -> None:
+        sys.stdout.write(message)
         sys.stdout.flush()
 
-    # start the current node's relayer part
+    # start the relayer
     relayer_thread = threading.Thread(target = relayer, args = (lock, neighbors, cli_output))
     relayer_thread.start()
     while True:
-        content = input()
-        if content == '':
+        line = input()
+        if line == '':
             stop_and_wait(lock, listener_thread, relayer_thread)
             return
         else:
-            message = make_header(name) + content
+            message = make_header(name) + line + '\n'
             data = make_packet(message.encode())
             with lock:
-                sys.stdout.write(message + '\n')
-                sys.stdout.flush()
+                cli_output(message)
                 for neighbor in neighbors:
                     neighbor.write_buffer += data  # send data to all neighbor nodes
 
-def start_node(name: str, my_addr: tuple[str, int],
-               inviter_addr: typing.Union[None, tuple[str, int]], option: str) -> None:
+def start_node(name: str, my_address: tuple[str, int],
+               inviter_address: typing.Union[None, tuple[str, int]], option: str) -> None:
     # prepare the neighbor list to be shared by all threads of this node
     neighbors = []
     # try to connect to the inviter
-    if inviter_addr:
+    if inviter_address:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         client_socket.settimeout(SOCKET_CONNECTION_TIMEOUT_SECONDS)
         try:
-            client_socket.connect(inviter_addr)
+            client_socket.connect(inviter_address)
         except socket.timeout:
-            sys.exit(make_header() + f'connection to the inviter ({inviter_addr}) timed out')
-        dump_to_stderr(f'connected to the inviter ({inviter_addr})\n')
+            sys.exit(make_header() + f'connection to the inviter ({inviter_address}) timed out')
+        dump_to_stderr(make_header() + f'connected to the inviter ({inviter_address})\n')
         client_socket.settimeout(SOCKET_OPERATION_TIMEOUT_SECONDS)
-        neighbors.append(Neighbor(client_socket, inviter_addr, b'', b''))
-    # start the current node's listener part
+        neighbors.append(Neighbor(client_socket, inviter_address, b'', b''))
+    # start the listener
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.settimeout(SOCKET_OPERATION_TIMEOUT_SECONDS)
-    server_socket.bind(my_addr)
+    server_socket.bind(my_address)
     server_socket.listen()  # this does not need to block even if "setblocking(True)"
     lock = threading.Lock()
     listener_thread = threading.Thread(target = listener, args = (lock, server_socket, neighbors))
@@ -296,6 +296,6 @@ if __name__ == '__main__':
     # TODO: add some argument checks
     option = sys.argv[1]
     name = sys.argv[2]
-    my_addr = (sys.argv[3], int(sys.argv[4]))
-    inviter_addr = None if len(sys.argv) == 5 else (sys.argv[5], int(sys.argv[6]))
-    start_node(name, my_addr, inviter_addr, option)
+    my_address = (sys.argv[3], int(sys.argv[4]))
+    inviter_address = None if len(sys.argv) == 5 else (sys.argv[5], int(sys.argv[6]))
+    start_node(name, my_address, inviter_address, option)
--- test.py
+++ test.py
@@ -17,11 +17,19 @@ def write_line(p: subprocess.Popen, s: str) -> None:
     p.stdin.flush()
 
 def verify_out_line(p: subprocess.Popen, s: str) -> None:
+    print('.', end = '', flush = True)
     if s not in p.stdout.readline():
         raise P2PTreeTestError()
 
 def verify_err_line(p: subprocess.Popen, s: str) -> None:
+    print('.', end = '', flush = True)
     if s not in p.stderr.readline():
+        raise P2PTreeTestError()
+
+def verify_termination(p: subprocess.Popen) -> None:
+    print('.', end = '', flush = True)
+    if p.poll() is None:
+        p.kill()
         raise P2PTreeTestError()
 
 def nap() -> None:
@@ -44,9 +52,7 @@ def p2p_tree_test_1() -> bool:
         verify_out_line(a, 'This is A')
         write_line(a, '')
         nap()
-        if a.poll() is None:
-            a.kill()
-            raise P2PTreeTestError()
+        verify_termination(a)
         return True
     except P2PTreeTestError:
         finalize(a)
@@ -69,14 +75,10 @@ def p2p_tree_test_2() -> bool:
         write_line(b, '')
         verify_err_line(a, 'detected')
         nap()
-        if b.poll() is None:
-            b.kill()
-            raise P2PTreeTestError()
+        verify_termination(b)
         write_line(a, '')
         nap()
-        if a.poll() is None:
-            a.kill()
-            raise P2PTreeTestError()
+        verify_termination(a)
         return True
     except P2PTreeTestError:
         finalize(a, b)
@@ -110,30 +112,140 @@ def p2p_tree_test_3() -> bool:
         verify_err_line(a, 'detected')
         verify_err_line(c, 'detected')
         nap()
-        if b.poll() is None:
-            b.kill()
-            raise P2PTreeTestError()
+        verify_termination(b)
         write_line(a, '')
         nap()
-        if a.poll() is None:
-            a.kill()
-            raise P2PTreeTestError()
+        verify_termination(a)
         write_line(c, '')
         nap()
-        if c.poll() is None:
-            c.kill()
-            raise P2PTreeTestError()
+        verify_termination(c)
         return True
     except P2PTreeTestError:
         finalize(a, b, c)
+        return False
+
+def p2p_tree_test_4() -> bool:
+    try:
+        a = launch(['A', '127.0.0.1', '10001'])
+        nap()
+        b = launch(['B', '127.0.0.1', '10002', '127.0.0.1', '10001'])
+        nap()
+        verify_err_line(a, 'accepted')
+        verify_err_line(b, 'connected')
+        c = launch(['C', '127.0.0.1', '10003', '127.0.0.1', '10001'])
+        nap()
+        verify_err_line(a, 'accepted')
+        verify_err_line(c, 'connected')
+        d = launch(['D', '127.0.0.1', '10004', '127.0.0.1', '10001'])
+        nap()
+        verify_err_line(a, 'accepted')
+        verify_err_line(d, 'connected')
+        write_line(a, 'This is A')
+        verify_out_line(a, 'This is A')
+        verify_out_line(b, 'This is A')
+        verify_out_line(c, 'This is A')
+        verify_out_line(d, 'This is A')
+        write_line(b, 'This is B')
+        verify_out_line(a, 'This is B')
+        verify_out_line(b, 'This is B')
+        verify_out_line(c, 'This is B')
+        verify_out_line(d, 'This is B')
+        write_line(c, 'This is C')
+        verify_out_line(a, 'This is C')
+        verify_out_line(b, 'This is C')
+        verify_out_line(c, 'This is C')
+        verify_out_line(d, 'This is C')
+        write_line(d, 'This is D')
+        verify_out_line(a, 'This is D')
+        verify_out_line(b, 'This is D')
+        verify_out_line(c, 'This is D')
+        verify_out_line(d, 'This is D')
+        write_line(b, '')
+        verify_err_line(a, 'detected')
+        write_line(c, '')
+        verify_err_line(a, 'detected')
+        write_line(d, '')
+        verify_err_line(a, 'detected')
+        nap()
+        verify_termination(b)
+        verify_termination(c)
+        verify_termination(d)
+        write_line(a, '')
+        nap()
+        verify_termination(a)
+        return True
+    except P2PTreeTestError:
+        finalize(a, b, c, d)
+        return False
+
+def p2p_tree_test_5() -> bool:
+    try:
+        a = launch(['A', '127.0.0.1', '10001'])
+        nap()
+        b = launch(['B', '127.0.0.1', '10002', '127.0.0.1', '10001'])
+        nap()
+        verify_err_line(a, 'accepted')
+        verify_err_line(b, 'connected')
+        c = launch(['C', '127.0.0.1', '10003', '127.0.0.1', '10002'])
+        nap()
+        verify_err_line(b, 'accepted')
+        verify_err_line(c, 'connected')
+        d = launch(['D', '127.0.0.1', '10004', '127.0.0.1', '10003'])
+        nap()
+        verify_err_line(c, 'accepted')
+        verify_err_line(d, 'connected')
+        e = launch(['E', '127.0.0.1', '10005', '127.0.0.1', '10004'])
+        nap()
+        verify_err_line(d, 'accepted')
+        verify_err_line(e, 'connected')
+        write_line(a, 'This is A')
+        verify_out_line(a, 'This is A')
+        verify_out_line(b, 'This is A')
+        verify_out_line(c, 'This is A')
+        verify_out_line(d, 'This is A')
+        verify_out_line(e, 'This is A')
+        write_line(e, 'This is E')
+        verify_out_line(a, 'This is E')
+        verify_out_line(b, 'This is E')
+        verify_out_line(c, 'This is E')
+        verify_out_line(d, 'This is E')
+        verify_out_line(e, 'This is E')
+        write_line(c, '')
+        verify_err_line(b, 'detected')
+        verify_err_line(d, 'detected')
+        nap()
+        verify_termination(c)
+        write_line(a, 'This is A again')
+        verify_out_line(a, 'This is A again')
+        verify_out_line(b, 'This is A again')
+        write_line(e, 'This is E again')
+        verify_out_line(d, 'This is E again')
+        verify_out_line(e, 'This is E again')
+        write_line(a, '')
+        verify_err_line(b, 'detected')
+        nap()
+        verify_termination(a)
+        write_line(b, '')
+        nap()
+        verify_termination(b)
+        write_line(e, '')
+        verify_err_line(d, 'detected')
+        nap()
+        verify_termination(e)
+        write_line(d, '')
+        nap()
+        verify_termination(d)
+        return True
+    except P2PTreeTestError:
+        finalize(a, b, c, d, e)
         return False
 
 if __name__ == '__main__':
     global_dict = globals().copy()
     for k, v in global_dict.items():
         if k.startswith('p2p_tree_test'):
-            print(f'running test {k}...')
+            print(f'running test {k} ', end = '', flush = True)
             if v():
-                print(f'test {k} passed')
+                print(f' test {k} passed')
             else:
-                sys.exit(f'test {k} failed')
+                sys.exit(f' test {k} failed')
